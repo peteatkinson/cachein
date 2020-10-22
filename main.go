fdc5a2c619110570d94e775ad7f63c6b8a02eb7b
Peter <peteratkinson1994@gmail.com>
2020-10-23 00:40:47 +0100

    midway commit

--- main.go
+++ main.go
@@ -23,12 +23,30 @@ func main() {
 		}
 
 		cache.Add("test1",employee)
-
+		
 		value, ok := cache.Get("test1")
 
 		if value != nil && ok == true {
 			fmt.Println(ok)
 			fmt.Println(value)
 		}
+
+
+		cache.Add("test2", &Employee{
+			123,
+			"James",
+		})
+
+		value2, ok2 := cache.Get("test2")
+
+		if(value2 != nil && ok2 == true) {
+			value, ok = cache.Get("test1")
+
+			if(value == nil && ok == false) {
+				fmt.Println(value)
+				fmt.Println(ok)
+			}
+		}
+
 	}
 }
